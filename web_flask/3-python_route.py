8fdf5e69912b486cef0376fc17517cdc78c6e811
Holbiwan <sabrinapapeau@gmail.com>
2023-12-12 18:06:36 +0100

    Create 3-python_route.py file

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Start a flask web application on localhost"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """Display HBNB!"""
+    return 'HBNB!'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def dynamic_text(text=None):
+    """dynamic routing"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_dynamic(text='is_cool'):
+    """dynamic w/ defaults"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
