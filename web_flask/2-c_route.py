2db5816ac1179412da80c7a743713e69314a21be
Holbiwan <sabrinapapeau@gmail.com>
2023-12-12 14:11:33 +0100

    Script called 2-c_route.py start a Flask web app

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Start a flask web application on localhost
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_bhnb():
+    """Display message Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display message HBNB"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
