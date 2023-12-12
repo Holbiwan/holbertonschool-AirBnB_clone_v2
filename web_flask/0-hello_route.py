b03cb14719850dd4fb1115478c4d417be8bf6655
Holbiwan <sabrinapapeau@gmail.com>
2023-12-12 16:36:18 +0100

    Fix indendation issues

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,10 +8,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-"""Display a message when making a request to root"""
-return 'Hello HBNB!'
-
+    """Display a message when making a request to root"""
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
     """run app flask"""
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
