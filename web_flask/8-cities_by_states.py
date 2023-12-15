3400d7e14c86b7e0d96bacedb64e2f9b7eab4726
Holbiwan <sabrinapapeau@gmail.com>
2023-12-15 04:18:13 +0100

    Modify app.teardown code

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""Start a Flask application on localhost."""
+"""Starts a Flask application on localhost"""
 
 from models import storage
 from flask import Flask, render_template
-from models.state
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def appcontext_teardown(exc=None):
-    """Called on teardown of app contexts.
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Check input data using a template"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
-    After each request: remove the current SQLAlchemy session.
-    """
+
+@app.teardown_appcontext
+def remove_session(exception):
+    """After each request remove current SQLAlchemy Session"""
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """Display a list of states and their cities"""
-
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=sorted(states))
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
