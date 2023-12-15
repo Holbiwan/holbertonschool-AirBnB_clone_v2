450d0cc9b38f5a4e64ac8cc9310ea4f210e18f34
Holbiwan <sabrinapapeau@gmail.com>
2023-12-15 16:08:56 +0100

    Modify code

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-"""Start a flask web application on localhost"""
+"""Starts a Flask web application on localhost"""
+
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 from models.amenity import Amenity
 
 app = Flask(__name__)
 
 
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """Display a HTML page like 6-index.html"""
+
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+
+
 @app.teardown_appcontext
-def close(exception):
+def close_storage(exception):
+    """Close the storage"""
     storage.close()
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
-    """ display the states filters """
-    states = storage.all(State)
-    amenities = storage.all(Amenity)
-
-    return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
