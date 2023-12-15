fc4e8939cb0ac92a8a8b2a30c9952c06022a9115
Holbiwan <sabrinapapeau@gmail.com>
2023-12-15 13:15:51 +0100

    Update code

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""Starts a Flask application on localhost"""
+"""Start a web app"""
 
-from models import storage
 from flask import Flask, render_template
+from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
+@app.route('/cities_by_states')
 def cities_by_states():
-    """Display a HTML page with a list of states and their cities"""
-    states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states)
+    """render cities by states sorted by name"""
+    path = '8-cities_by_states.html'
+    states = storage.all(State)
+    return render_template(path, states=states)
 
 
 @app.teardown_appcontext
-def app_tear_down(arg=None):
-    """After each request, remove the current SQLAlchemy Session"""
+def app_teardown(arg=None):
+    """Clean-up session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
