a6c270955f799ab17f3a0148d42ef07b9ca885ae
Holbiwan <sabrinapapeau@gmail.com>
2023-12-15 18:52:06 +0100

    Review code

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""
-Start a Flask application on localhost"""
+"""Starts a Flask web application on localhost"""
 
 from models import storage
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-def teardown_appcontext(exc=None):
-    """
-    Called on teardown of app contexts,
-    for more info on contexts visit
-    -> http://flask.pocoo.org/docs/1.0/appcontext/
-    Storage.close() closes the SQL scoped session or reloads file storage.
-    """
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Displays the main HBnB filters HTML page."""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
-app.teardown_appcontext(teardown_appcontext)
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
-    """
-    Checking input data using templating
-    """
-    states = sorted(storage.all("State").values(), key=lambda x: x.name)
-    amenities = sorted(storage.all("Amenity").values(), key=lambda x: x.name)
-
-    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
