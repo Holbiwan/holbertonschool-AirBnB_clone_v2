3d46edcbaf8b379cd8c490b8a6aaf90fa26e1ff0
(Saby)  HOLBIWAN <sabrinapapeau@gmail.com>
2023-12-15 05:29:47 +0100

    Add module and modify syntax

--- models/state.py
+++ models/state.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""State Module for HBNB project."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
-from os import getenv
 import models
 import shlex
-import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
 
 
 class State(BaseModel, Base):
-    """table of the class State"""
+    """Table for the State class.
+
+    Attributes:
+        name (str): The name of the state.
+        cities (relationship): Relationship to City model.
+    """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    cities_relation = relationship("City", cascade='all, delete, delete-orphan',
+                                   backref="state")
 
     @property
     def cities(self):
-        """return the cities of the current state"""
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+        """Return the cities of the current state."""
+        all_cities = models.storage.all(City)
+        state_cities = [city for city in all_cities.values() if city.state_id == self.id]
+        return state_cities
