4bb75deed778aed57cb4a8b2d756ab33428ce572
(Saby)  HOLBIWAN <sabrinapapeau@gmail.com>
2023-12-15 05:26:06 +0100

    Import modules and correct  syntax

--- models/city.py
+++ models/city.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel, Base
+"""City Module for HBNB project."""
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
-    """This is the class for City
+    """
+    City class for storing city information.
+
     Attributes:
-        state_id: The state id
-        name: input name
+        name (str): The name of the city.
+        state_id (str): The state id.
+        places (relationship): Relationship to Place model.
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", cascade='all, delete, delete-orphan',
                           backref="cities")
+
+    def __init__(self, *args, **kwargs):
+        """Initialize City instance."""
+        super().__init__(*args, **kwargs)
